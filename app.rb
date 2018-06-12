d1746b8f2a051e4afc109f3c347106ca160855d1
sirikops <sirikops@hotmail.com>
2018-06-12 21:53:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Started my server using Shotgun!"
   end
 
 end
+
+#shotgun doesn't require to refresh
+#rackup requires refresh to display changes
